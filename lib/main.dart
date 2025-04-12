50af85b823191f179766bd5abd653d3f1ad9e8b6
pvminh94 <phanvanminh0203@gmail.com>
2025-04-12 22:16:08 +0700

    2_commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:crm_wookroom/presentation/pages/dashboards/a_dashboard.dart';
 
 void main() {
-  runApp(const MainApp());
+  runApp(const MyApp());
 }
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
-      ),
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'CRM Wookroom',
+      home: const ADashboard(), // gọi Dashboard ở đây
     );
   }
 }
